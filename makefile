af843d632100185b977b00805dc7250f1554d1dc
Brian Kung <brian@callmekung.com>
2013-04-13 16:38:43 -0500

    Pre-setup files

---
+++ makefile
@@ -0,0 +1,6 @@
+# Pong Makefile for GCC
+
+all: helloworld
+
+helloworld:
+	g++ ./src/HelloWorld.cpp -o hello_world
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include ""
+
+using namespace sf;
+
+int main(int argc, char const *argv[])
+{
+	printf("Hello World!");
+	return 0;
+}

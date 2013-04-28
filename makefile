7774b45b725827134c3a7fec85413bde242c2a7d
Daniel Kim <DanielKim.7c4@gmail.com>
2013-04-27 23:40:33 -0500

    - Makefile creates a directory to put the Linux-compiled binary - Makefile now links against SFML - Game will now open a window and draw a 10-pixel box at the top left of the window

--- makefile
+++ makefile
@@ -1,6 +1,14 @@
 # Pong Makefile for GCC
 
-all: helloworld
+INCLUDE = -I ./include
 
-helloworld:
-	g++ ./src/HelloWorld.cpp -o hello_world
+LFLAGS = -lsfml-graphics -lsfml-window -lsfml-system
+LIBS = ./lib/linux/libsfml-graphics.so.1.6
+LIBS += ./lib/linux/libsfml-system.so.1.6
+LIBS += ./lib/linux/libsfml-window.so.1.6
+
+all: pong
+
+pong: $(LIBS)
+	mkdir -p ./bin/linux/
+	g++ ./src/main.cpp -o ./bin/linux/pong $(LFLAGS)
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,36 @@
 #include <stdio.h>
-#include ""
-
-using namespace sf;
+#include "SFML/Graphics.hpp"
 
 int main(int argc, char const *argv[])
 {
-	printf("Hello World!");
+	sf::RenderWindow window(sf::VideoMode(800, 600), "PONG");
+
+	while (window.IsOpened()) {
+		sf::Event event;
+
+		// Process any input events
+		while (window.GetEvent(event)) {
+			// Quit game if the window was closed or if the escape key was pressed
+			if ((event.Type == sf::Event::Closed) ||
+				((event.Type == sf::Event::KeyPressed) && (event.Key.Code == sf::Key::Escape)))
+			{
+				window.Close();
+				break;
+			}
+		}
+
+		// TODO: Limit framerate to make it easier on processors (max 60fps?)
+		window.Clear();
+
+		// Do drawing here
+		// Drawing a rectangle
+		sf::Color rectangleColor = sf::Color(255, 255, 255);	// Color it white!
+		sf::Shape rectangle = sf::Shape::Rectangle(0.0f, 0.0f, 10.0f, 10.0f, rectangleColor);
+		window.Draw(rectangle);
+
+		// Display the drawn frame in the window
+		window.Display();
+	}
+
 	return 0;
 }
